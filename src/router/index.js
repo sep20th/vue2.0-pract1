2cab2decd0b056d83ef2616b6ca27ed529184be4
sep20th <sep20th@qq.com>
2017-05-13 08:36:54 +0800

    添加路由-未完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,41 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import IndexPage from '../pages/index'
-
+import DetailPage from "../pages/detail"
+import DetailAna from "../pages/detail/analysis"
+import DetailCou from "../pages/detail/count"
+import DetailFor from "../pages/detail/forecast"
+import DetailPub from "../pages/detail/publish"
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-			path:'/',
-			component:IndexPage
-		}
+		path:'/',
+		component:IndexPage
+	},
+	{
+		path:'/detail',
+		component:DetailPage,
+		children:[
+			{
+				path:"analysis",
+				component:DetailAna
+			},
+			{
+				path:'count',
+				component:DetailCou
+			},
+			{
+				path:'forecast',
+				component:DetailFor
+			},
+			{
+				path:'publish',
+				component:DetailPub
+			}
+		]
+	}
   ]
 })
